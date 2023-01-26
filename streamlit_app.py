59acab1fab585f2a82d5c2bdf7516853c430e683
RoshiniFernando <47807830+RoshiniFernando@users.noreply.github.com>
2023-01-26 22:32:25 +0530

    set page config, logo image and title

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+# Import for API calls
+import requests
+
+# Import for navbar
+from streamlit_option_menu import option_menu
+
+# Import for dynamic tagging
+from streamlit_tags import st_tags, st_tags_sidebar
+
+# Imports for aggrid
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from st_aggrid import GridUpdateMode, DataReturnMode
+
+# Import for loading interactive keyboard shortcuts into the app
+#from dashboard_utils.gui import keyboard_to_url
+#from dashboard_utils.gui import load_keyboard_class
+
+st.set_page_config(layout="wide", page_title="Zero-Shot Text Classifier", page_icon="🤗")
+
+st.image("logo.png", width=100)
+
+st.title("Zero-Shot Text Classifier")
